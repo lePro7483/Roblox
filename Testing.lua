a67440ae6869a3d6ec0e6f9283f3deb372fca56a
lePro7483 <100308543+lePro7483@users.noreply.github.com>
2022-05-17 18:38:26 +0200

    Update Testing.lua

--- Testing.lua
+++ Testing.lua
@@ -5,5 +5,6 @@ for i,v in pairs(TableAndChair) do
  if v == game.Players.LocalPlayer.Name then
   print(v.." is Premium")
   game.Players.LocalPlayer.Character.EASportsLoveMe.Value = true
+  print("Toggled")
  end
 end
