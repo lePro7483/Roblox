347a0cf93ba8f11bab0232e1c8fd481114b8b5a5
lePro7483 <100308543+lePro7483@users.noreply.github.com>
2022-05-17 18:35:35 +0200

    Update Testing.lua

--- Testing.lua
+++ Testing.lua
@@ -4,5 +4,6 @@ print("Ran CODE")
 for i,v in pairs(TableAndChair) do
  if v == game.Players.LocalPlayer.Name then
   print(v.." is Premium")
+  game.Players.LocalPlayer.Character.EASportsLoveMe.Value = true
  end
 end
