0044cf5370c8e8153e35c6507ee9a0844089618a
lePro7483 <100308543+lePro7483@users.noreply.github.com>
2022-05-26 00:37:07 +0200

    Update and rename KHUB Univ v1.lua to KHubUnivFR.lua

--- KHubUnivFR.lua
+++ KHubUnivFR.lua
@@ -2,37 +2,37 @@
 -- I use Orion lib by shlex, you can find it here : https://github.com/shlexware/Orion/blob/main/Documentation.md
 local ver = 0.2
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-OrionLib:MakeNotification({Name = "Loaded KHub Univ", Content = "If probs : DeadInside#5302", Image = "rbxassetid://0", Time = 7})
+OrionLib:MakeNotification({Name = "KHub Univ Lancé", Content = "Si Probs : DeadInside#5302", Image = "rbxassetid://0", Time = 7})
 local Window = OrionLib:MakeWindow({Name = "KHub Univ Gui v"..ver.." *Test*", HidePremium = true, SaveConfig = false, ConfigFolder = "N/A"})
 local LPTab = Window:MakeTab({Name = "LocalPlayer", Icon = "rbxassetid://9657052086", PremiumOnly = false})
-LPTab:AddLabel("*BETA* Some assets may change : DeadInside#5302")
+LPTab:AddLabel("*BETA* Des assets peuvent changer : DeadInside#5302")
 
 LPTab:AddSlider({
-	Name = "WalkSpeed",
+	Name = "Vitesse",
 	Min = 1,
 	Max = 100,
 	Default = 16,
 	Color = Color3.fromRGB(200,200,200),
 	Increment = 1,
-	ValueName = "Speed",
+	ValueName = "Vitesse",
 	Callback = function(Value)
 		game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
 	end    
 })
 LPTab:AddSlider({
-	Name = "JumpPower",
+	Name = "Saut",
 	Min = 25,
 	Max = 200,
 	Default = 50,
 	Color = Color3.fromRGB(200,200,200),
 	Increment = 1,
-	ValueName = "JumpPower",
+	ValueName = "Saut",
 	Callback = function(Value)
 		game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
 	end    
 })
 LPTab:AddButton({
-	Name = "Inf Jump (Jump bruv)",
+	Name = "Saut Infini (Sauter)",
 	Callback = function()
 		game:GetService("UserInputService").JumpRequest:connect(function()
 		game:GetService"Players".LocalPlayer.Character:FindFirstChildOfClass'Humanoid':ChangeState("Jumping")
@@ -40,25 +40,25 @@ LPTab:AddButton({
 	end
 })
 LPTab:AddButton({
-	Name = "CFly + Noclip (E to toggle, bypasses most AC)",
+	Name = "CFly + Noclip (E pour Activer/Desactiver)",
 	Callback = function()
       	loadstring(game:HttpGet("https://raw.githubusercontent.com/LegitH3x0R/Roblox-Scripts/main/AEBypassing/CFrameFly.lua"))()
   	end    
 })
 LPTab:AddButton({
-	Name = "Broken GodMod (Toggle with E)",
+	Name = "GodMod Casser (E pour Activer/Desactiver)",
 	Callback = function()
       	loadstring(game:HttpGet("https://www.pastebin.com/raw/sUCpkFhG", true))()
   	end    
 })
 LPTab:AddButton({
-	Name = "ESP *WIP* (Using Sirius ESP Lib)",
+	Name = "ESP *WIP* (Utilisant Sirius ESP Lib)",
 	Callback = function()
-      	OrionLib:MakeNotification({Name = "Why? Just WHY?", Content = "Just said it was WIP...", Image = "rbxassetid://0", Time = 7})
+      	OrionLib:MakeNotification({Name = "MAIS POURQUOI?", Content = "JE VIENS DE DIRE C WIP", Image = "rbxassetid://0", Time = 7})
   	end    
 })
 
-local SCTab = Window:MakeTab({Name = "Useful Scripts", Icon = "rbxassetid://0", PremiumOnly = false})
+local SCTab = Window:MakeTab({Name = "Scripts Utiles", Icon = "rbxassetid://0", PremiumOnly = false})
 SCTab:AddButton({
 	Name = "Brookhaven",
 	Callback = function()
@@ -72,45 +72,45 @@ SCTab:AddButton({
   	end
 })
 SCTab:AddButton({
-	Name = "SpyMP",
+	Name = "Espion MP",
 	Callback = function()
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/lePro7483/Roblox/main/SpyMP.lua"))()
   	end
 })
 SCTab:AddButton({
-	Name = "DisplaySpyMP",
+	Name = "Display Espion MP",
 	Callback = function()
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/lePro7483/Roblox/main/DisplaySpyMP.lua"))()
   	end
 })
 SCTab:AddButton({
-	Name = "UltimateFling",
+	Name = "Fling Ultime",
 	Callback = function()
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/lePro7483/Roblox/main/UltimateFling.lua"))()
   	end
 })
 SCTab:AddButton({
-	Name = "Universal Car Modifier",
+	Name = "Modificateur de Vehicule Universel",
 	Callback = function()
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/lePro7483/Roblox/main/UniversalCarModif.lua"))()
   	end
 })
 SCTab:AddButton({
-	Name = "Some Info",
+	Name = "Des Infos",
 	Callback = function()
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/lePro7483/Roblox/main/SomeInfo.lua"))()
   	end
 })
 
-local WSCTab = Window:MakeTab({Name = "Weird Scripts", Icon = "rbxassetid://0", PremiumOnly = false})
+local WSCTab = Window:MakeTab({Name = "Scripts chelous", Icon = "rbxassetid://0", PremiumOnly = false})
 WSCTab:AddButton({
-	Name = "Say All Names",
+	Name = "Dire tout les noms",
 	Callback = function()
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/lePro7483/Roblox/main/SayAllNames.lua"))()
   	end
 })
 WSCTab:AddButton({
-	Name = "Book? *Open* / *Close*",
+	Name = "Livre? *Open* / *Close*",
 	Callback = function()
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/lePro7483/Roblox/main/BookUniversal.lua"))()
   	end
