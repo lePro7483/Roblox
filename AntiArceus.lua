cc09b79ee2c24cb5d08db82fb7a66832011c998c
lePro7483 <100308543+lePro7483@users.noreply.github.com>
2022-06-13 20:10:06 +0200

    Update AntiArceus.lua

--- AntiArceus.lua
+++ AntiArceus.lua
@@ -1,9 +1,14 @@
-local enctff = identifyexecutor()
-if encstff == "Arceus" then
+local encstff = _VERSION
+print(encstff)
+if encstff == 5.4 then
 	print("Detected Virus in Phone, Dumbass")
 	wait(5)
 	game.Players.LocalPlayer:Kick("Error using Arceus X, Please use a real executor, not a virus, Fuck Tiahh")
 	else
-	print("Using : "..encstff)
+	if not encstff == nil then
+		print("Using : "..encstff)
+	else
+		print("I probably broke it lmao")
+	end
 end
 print("Ended Verification of Intelligence Level of the Exploiter")
