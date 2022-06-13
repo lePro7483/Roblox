a4c8a143a8116d4ce5cc37713208f234619ff766
lePro7483 <100308543+lePro7483@users.noreply.github.com>
2022-06-13 19:52:29 +0200

    Add files via upload

---
+++ DoingStuff.lua
@@ -0,0 +1,11 @@
+local enckhubstff = identifyexecutor()
+if enckhubstff == "Arceus" then
+	print("Detected Virus in Phone, Dumbass")
+	wait(5)
+	game.Players.LocalPlayer:Kick("Error using Arceus X, Please use a real executor, not a virus, Fuck Tiahh")
+else
+	if Krnl then
+		print("Acceptable enough")
+	end
+end
+print("Ended Verification of Intelligence Level of the Exploiter")
