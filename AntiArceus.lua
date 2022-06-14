07a5fbb4a7b000778c0fc02651cd76e80a61860a
lePro7483 <100308543+lePro7483@users.noreply.github.com>
2022-06-14 14:06:21 +0200

    Update AntiArceus.lua

--- AntiArceus.lua
+++ AntiArceus.lua
@@ -1,8 +1,27 @@
-local encstff = _VERSION
-print(encstff)
-if not encstff == "Luau" then
-	print("Detected Virus in Phone, Dumbass")
-	wait(5)
-	game.Players.LocalPlayer:Kick("Error : Using Arceus X, Please use a real executor, Arceus is a Virus, Please search info about that")
+print("By https://v3rmillion.net/member.php?action=profile&uid=806967 \nAnd other ppl (imagine exploiting in Arceus X lmao)")
+local uis = game:GetService("UserInputService")
+local function isArceus()
+    local mobileDevice
+    local noConcatOperators
+   
+    local kbe = uis.KeyboardEnabled
+    local te = uis.TouchEnabled
+    if kbe == false and te == false then
+        mobileDevice = true
+    end
+    local succ,err = pcall(function()
+        loadstring("local x = '';x ..= 'a'")()
+    end)
+    if not succ then
+        noConcatOperators = true
+    end
+    if mobileDevice and noConcatOperators then
+        return true
+    else
+        return false
+    end
 end
-print("Ended Verification of Intelligence Level of the Exploiter")
+
+if isArceus() then
+    game:Shutdown()
+end
