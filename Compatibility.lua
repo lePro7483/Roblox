4f1028f3beb72e1fe063995815ed22f0bd913f0d
lePro7483 <100308543+lePro7483@users.noreply.github.com>
2022-07-06 15:31:15 +0200

    Update Compatibility.lua

--- Compatibility.lua
+++ Compatibility.lua
@@ -1,10 +1,11 @@
 if not isfile("StarLaggingFiles") then
 	makefolder("StarLaggingFiles")
 end
-_G.Compatible = isfile("coconut.jpg")
+_G.Compatible = isfile("StarLaggingFiles/coconut.jpg")
+local IE = identifyexecutor or identify_executor
 if _G.Compatible then
 	print("Exploit is compatible")
 else
 	while true do end
-  game.Players.LocalPlayer:Kick("Your exploit doesn't have required files, error1 (DM DeadInside#5302 for INFO)")
+	game.Players.LocalPlayer:Kick("Exploit not supported, DM DeadInside#5302 for Support")
 end
