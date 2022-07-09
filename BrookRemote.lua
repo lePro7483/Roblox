6294ec408a820ca3255e560e4d75cb692aa1c708
lePro7483 <100308543+lePro7483@users.noreply.github.com>
2022-07-09 23:00:59 +0200

    Update BrookRemote.lua

--- BrookRemote.lua
+++ BrookRemote.lua
@@ -1,1 +1,34 @@
-_G.BRem = game:GetService("ReplicatedStorage").Boo
+local tab = game.ReplicatedStorage:GetChildren()
+local blacklist = {"TempThemes","DefaultChatSystemChatEvents","PlayersStartup","SingleVehicles","MotelRegions","UiClone","ClonedTools","BannedLots","AvatarEditorScipts"}
+function FindFolder()
+	for i,v in pairs(tab) do
+		if not table.find(blacklist,v.Name) then
+			if v.ClassName == "Folder" then
+				local rf = v
+				return rf
+			end
+		end
+	end
+end
+function FindMain()
+	local fold = FindFolder()
+	if fold:FindFirstChildOfClass("Folder") then
+		fold = fold:FindFirstChildOfClass("Folder")
+		if fold:FindFirstChildOfClass("Folder") then
+			fold = fold:FindFirstChildOfClass("Folder")
+			if fold:FindFirstChildOfClass("Folder") then
+				fold = fold:FindFirstChildOfClass("Folder")
+			else
+				_G.BRem = fold
+				print(_G.BRem:GetFullName())
+			end
+		else
+			_G.BRem = fold
+			print(_G.BRem:GetFullName())
+		end
+	else
+	_G.BRem = fold
+	print(_G.BRem:GetFullName())
+	end
+end
+FindMain()
