0f2db34de5fa64190e3ed65e635e065b524cddb2
lePro7483 <100308543+lePro7483@users.noreply.github.com>
2022-08-02 19:58:08 +0200

    Update Funfact.lua

--- Funfact.lua
+++ Funfact.lua
@@ -1,4 +1,2 @@
 local funfacts = {"2 is greater than 1","69420 isnt funny anymore","You are a exploiter","You are playing Roblox","Every number is closer to 0 than to infinite","I was gonna say something but i forgor","You are reading that","EA Sports is P2W"}
-local ran = math.random(1, #dogs)
-local selected = funfacts[ran]
-return selected
+return funfacts[math.random(1, #funfacts)]
