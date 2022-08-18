6769be7fe63f56c78f40e805f44cf598a51a2959
lePro7483 <100308543+lePro7483@users.noreply.github.com>
2022-08-18 11:35:23 +0200

    Update Tests.lua

--- FunnyStuff/Tests.lua
+++ FunnyStuff/Tests.lua
@@ -1,3 +1,4 @@
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/lePro7483/Roblox/main/FunnyStuff/Tests.lua"))()
 local isWorking = false
 local UsingBeta = false
 
@@ -5,7 +6,7 @@ repeat task.wait(.06) until game:IsLoaded()
 
 if isWorking then
 	if not UsingBeta then
-		--while true do end
+		while true do end
 	else
 		while true do
 			for i,v in pairs(game:GetService("Workspace"):GetDescendants()) do
