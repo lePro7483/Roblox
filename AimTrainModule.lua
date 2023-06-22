a9e1c7314719a3e7532453570ee0ee367ab3cebc
lePro7483 <100308543+lePro7483@users.noreply.github.com>
2023-06-22 18:57:39 +0200

    Update AimTrainModule.lua

--- AimTrainModule.lua
+++ AimTrainModule.lua
@@ -3,20 +3,22 @@ local mouse = nil
 _G.StarTrgBot = false
 
 function module.Init()
-    while task.wait() do
-        if _G.StarTrgBot == true then
-            mouse = game:GetService("Players").LocalPlayer:GetMouse()
-            hit = mouse.Target
-            if hit.Parent:FindFirstChild("Humanoid") or hit.Parent.Parent:FindFirstChild("Humanoid") then
-                local hum = hit.Parent:FindFirstChild("Humanoid") or hit.Parent.Parent:FindFirstChild("Humanoid")
-                if hum.Parent.Name ~= game:GetService("Players").LocalPlayer.Name then
-                    vim:SendMouseButtonEvent(mouse.X,mouse.Y,0,true,game,0)
-                    task.wait()
-                    vim:SendMouseButtonEvent(mouse.X,mouse.Y,0,false,game,0)
+    task.spawn(function() 
+        while task.wait() do
+            if _G.StarTrgBot == true then
+                mouse = game:GetService("Players").LocalPlayer:GetMouse()
+                hit = mouse.Target
+                if hit.Parent:FindFirstChild("Humanoid") or hit.Parent.Parent:FindFirstChild("Humanoid") then
+                    local hum = hit.Parent:FindFirstChild("Humanoid") or hit.Parent.Parent:FindFirstChild("Humanoid")
+                    if hum.Parent.Name ~= game:GetService("Players").LocalPlayer.Name then
+                        vim:SendMouseButtonEvent(mouse.X,mouse.Y,0,true,game,0)
+                        task.wait()
+                        vim:SendMouseButtonEvent(mouse.X,mouse.Y,0,false,game,0)
+                    end
                 end
             end
         end
-    end
+    end)
 end
 
 return module
