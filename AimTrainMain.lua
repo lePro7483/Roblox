8f7b4fe237ffe10e160d7eb0474e85ccf93cff12
lePro7483 <100308543+lePro7483@users.noreply.github.com>
2023-06-22 18:47:24 +0200

    Update AimTrainMain.lua

--- AimTrainMain.lua
+++ AimTrainMain.lua
@@ -1,6 +1,7 @@
 local key = Enum.KeyCode.P
 local UIS = game:GetService("UserInputService")
-
+local module = loadstring(game:HttpGet("https://raw.githubusercontent.com/lePro7483/Roblox/main/AimTrainModule.lua"))()
+module.Init()
 UIS.InputBegan:Connect(function(key,gpe)
     if gpe then return end
     if key.KeyCode == key then
