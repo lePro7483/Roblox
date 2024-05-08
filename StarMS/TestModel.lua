538913aee33c823890ac11c631d5e76345a8f188
lePro7483 <100308543+lePro7483@users.noreply.github.com>
2024-05-08 16:38:18 +0200

    Update TestModel.lua

--- StarMS/TestModel.lua
+++ StarMS/TestModel.lua
@@ -1,5 +1,4 @@
 local parttarget = _G.StarMSmodel
-print(_G.StarMSmodel)
 for i,v in pairs(parttarget:GetChildren()) do
   for ib,vb in pairs(v:GetChildren()) do
 	  vb.Parent = game:GetService(v.Name)
