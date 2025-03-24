a856a2cb4e7290fcb7ca823e2840dffff52a6845
lePro7483 <100308543+lePro7483@users.noreply.github.com>
2025-03-24 17:12:14 +0100

    Update breathalyzer.lua

--- StarMS/breathalyzer.lua
+++ StarMS/breathalyzer.lua
@@ -1,1 +1,21 @@
-nil
+local parttarget = _G.StarMSmodel
+warn("Beginning extraction\n")
+parttarget.Breathalyzer.Parent = game:GetService("StarterGui")
+parttarget.StarAlSystem.Parent = game:GetService("ServerScriptService")
+parttarget.Bottle.Parent = game:GetService("StarterPack")
+parttarget.Breathalyzer.Parent = game:GetService("StarterPack")
+parttarget.CameraEffect.Parent = game:GetService("StarterPlayer").StarterPlayerScripts
+for i,v in pairs(parttarget.TempFold:GetChildren()) do
+  v.Parent = game:GetService("Lighting")
+end
+parttarget:Destroy()
+_G.StarMSmodel = nil
+
+print()
+warn("------------------------------")
+print(" ")
+print()
+warn("Finished unpacking Breathalyzer.rbxm")
+print(" ")
+print()
+warn("------------------------------")
