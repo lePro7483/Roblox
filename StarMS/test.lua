8da6c897bdf8b58c3bc5851551ade49286a5dd0e
lePro7483 <100308543+lePro7483@users.noreply.github.com>
2025-03-24 16:48:52 +0100

    Update test.lua

--- StarMS/test.lua
+++ StarMS/test.lua
@@ -1,2 +1,4 @@
 print("test was inputed, beginning setup for test.rbxm")
+print("selected model was ".._G.StarMSModel.Name)
+_G.StarMSModel.Parent = game:GetService("ReplicatedStorage")
 print("done")
