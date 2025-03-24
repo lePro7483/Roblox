ce533efe0b82569ee19e1345fb3ed83a51b4fb0d
lePro7483 <100308543+lePro7483@users.noreply.github.com>
2025-03-24 17:19:00 +0100

    Update breathalyzer.lua

--- StarMS/breathalyzer.lua
+++ StarMS/breathalyzer.lua
@@ -1,7 +1,7 @@
 local parttarget = _G.StarMSmodel
 warn("Beginning extraction\n")
 print(parttarget:GetFullName())
-parttarget.Breathalyzer.Parent = game:GetService("StarterGui")
+parttarget.BreathalyzerUI.Parent = game:GetService("StarterGui")
 parttarget.StarAlSystem.Parent = game:GetService("ServerScriptService")
 parttarget.Bottle.Parent = game:GetService("StarterPack")
 parttarget.Breathalyzer.Parent = game:GetService("StarterPack")
